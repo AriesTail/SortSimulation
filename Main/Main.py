fc3576044b8fe9322ca70f347a7e764dee484144
AriesT <a506400071@gmail.com>
2019-10-23 11:41:58 -0700

    .

--- Main/Main.py
+++ Main/Main.py
@@ -3,6 +3,7 @@ import random
 import re
 import sys
 
+from matplotlib import pyplot
 import pandas
 from pandas.core.frame import DataFrame
 
@@ -11,6 +12,31 @@ from Sort.Sorts import SortMethonds
 sys.setrecursionlimit(100000000)
 address = "../datasets/"
 n = 20000
+
+
+def generateGraph():
+    for datatypes in os.listdir(address):
+        for dataforms in os.listdir(address + datatypes):
+            for file in os.listdir(address + datatypes + "/" + dataforms):
+                if file == "result.csv":
+                    pyplot.cla()
+                    result = pandas.read_csv(address + datatypes + "/" + dataforms + "/" + "result.csv")
+                    x = list(result.columns.values)[2:]
+                    ys = result.iloc[:, 1:]
+                    for i in range(ys.shape[0]):
+                        y = ys.iloc[i]
+                        pyplot.plot(x, y.tolist()[1:])
+                    pyplot.xlabel(dataforms)
+                    pyplot.ylabel("Time")
+                    pyplot.title(datatypes + " " + dataforms)
+                    pyplot.legend(ys.iloc[:, 0].tolist())
+#                     pyplot.show()
+                    try:
+                        os.remove(address + datatypes + "/" + dataforms + "/Figure.png")
+                    except:
+                        None
+                    pyplot.savefig(address + datatypes + "/" + dataforms + "/Figure.png")
+                    
 
 
 def readFile():
@@ -55,14 +81,12 @@ def analyse(data, dataSize, result):
     sortMethonds.sortAll()
 
 
-datas = readFile()
+generateGraph()
+
+# datas = readFile()
 # randomGenerator()
 # orderedGenerator()
-while True:
-#     try:
-        data, dataSize, result = next(datas)
-        analyse(pandas.read_csv(data, header=None, sep="\t").iloc[0].tolist(), dataSize, result)
-#     except StopIteration as e:
-#         print('Generator return value:', e.value)
-#         break
+# while True:
+#     data, dataSize, result = next(datas)
+#     analyse(pandas.read_csv(data, header=None, sep="\t").iloc[0].tolist(), dataSize, result)
 
